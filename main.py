fedf5f96340c49c1d05fb82841dfd4004d7ab50f
Haavard Zackarias Pettersen <hzpetter@gmail.com>
2025-01-03 23:20:20 +0100

    experimenting in main

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world again")
+
+import numpy as np
+import matplotlib.pyplot as plt
+from coord_translation import topol, tocart
+from functions import Mintegral, SMintegral
+
+def f(x):
+    return 1/(x+1)**2
+
+mesh = np.linspace(0, 100, 1000)
+
+fspace = f(mesh)
+
+integ = SMintegral(f, mesh)
+
+abs = fspace/integ
+
+plt.plot(mesh, abs)
+plt.plot(mesh, fspace)
+plt.show()
+
