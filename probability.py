d143789e008fe6704d9d4d62ae211136c08f6939
Haavard Zackarias Pettersen <hzpetter@gmail.com>
2025-01-04 15:10:17 +0100

    adde boolout function

--- probability.py
+++ probability.py
@@ -1,3 +1,13 @@
-
 import numpy as np
+import random as rp
 
+def boolout(P):
+    p = str("0.")
+    for i in range(len(str(P)) - 2):
+        p += str(rp.randint(0, 9))
+    print(p)
+    p = float(p)
+    if p <= P:
+        return True
+    elif p > P:
+        return False
