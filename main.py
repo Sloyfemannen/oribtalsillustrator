b9decdcee22b95ab7b64ba4fe6c878f347ab98c3
Haavard Zackarias Pettersen <hzpetter@gmail.com>
2025-01-04 13:28:51 +0100

    more experiments in main, working on absoluting functions on meshes

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import random as rd
 from coord_translation import topol, tocart
 from functions import Mintegral, SMintegral
+
+L = 10
+
+N = 100 * L
 
 def f(x):
     return 1/(x+1)**2
 
-mesh = np.linspace(0, 100, 1000)
+mesh = np.linspace(0, L, N)
 
 fspace = f(mesh)
 
-integ = SMintegral(f, mesh)
+integ = fspace / sum(fspace)
 
-abs = fspace/integ
+abs = fspace * integ
+
+#abs = abs / abs[0]
 
 plt.plot(mesh, abs)
-plt.plot(mesh, fspace)
+#plt.plot(mesh, fspace)
 plt.show()
 
