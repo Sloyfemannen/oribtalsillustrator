600249cef5f1552ff5b145769615ea77f27b46c0
Sloyfemannen <124448585+Sloyfemannen@users.noreply.github.com>
2025-01-04 22:15:37 +0100

    added floatint function with regex library

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import regex as rg
 
 def integral(f, x, y, N):
     m = np.linspace(x, y, N)
@@ -13,3 +14,8 @@ def Mintegral(f, mesh):
 def SMintegral(f, mesh):
     dx = (mesh[-1]-mesh[0])/len(mesh)
     return sum(f(mesh + dx/2) * dx)
+
+def floatint(n):
+    N = str(n)
+    N = rg.sub('[.]', '', N)
+    return int(N)
