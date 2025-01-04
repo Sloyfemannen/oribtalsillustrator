68b8a040f8bed64131776f52f85049d71bca359c
Haavard Zackarias Pettersen <hzpetter@gmail.com>
2025-01-04 13:21:06 +0100

    changed integral to take the middle integral instead of the first integral

--- functions.py
+++ functions.py
@@ -4,12 +4,12 @@ import numpy as np
 def integral(f, x, y, N):
     m = np.linspace(x, y, N)
     dx = (y-x)/N
-    return f(m) * dx
+    return f(m + dx/2) * dx
 
 def Mintegral(f, mesh):
     dx = (mesh[-1]-mesh[0])/len(mesh)
-    return f(mesh) * dx
+    return f(mesh  + dx/2) * dx
 
 def SMintegral(f, mesh):
     dx = (mesh[-1]-mesh[0])/len(mesh)
-    return sum(f(mesh) * dx)
+    return sum(f(mesh + dx/2) * dx)
