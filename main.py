f8c2067f41d6a7034f3a40a9384820efd9366b74
Sloyfemannen <124448585+Sloyfemannen@users.noreply.github.com>
2025-01-05 10:56:43 +0100

    added error function

--- main.py
+++ main.py
@@ -21,15 +21,20 @@ abs = fspace / sum(fspace)
 
 barlist = [0 for i in mesh]
 
-for j in range(1000):
+for j in range(2000):
     for i, e in enumerate(abs):
         test = boolout(e)
         if test == True:
             barlist[i] += 1
 
-barlist = np.array(barlist)
+m = max(barlist)
+
+barlist = np.array(barlist) / m * max(abs)
+
+err = np.sqrt((barlist-abs)**2)
 
 plt.plot(mesh, abs)
-plt.show()
 plt.plot(mesh, barlist)
+plt.show()
+plt.plot(mesh, err)
 plt.show()
