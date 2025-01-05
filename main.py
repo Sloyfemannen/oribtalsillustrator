536c782f06135b09c3392acf036084941d1e4db0
Sloyfemannen <124448585+Sloyfemannen@users.noreply.github.com>
2025-01-05 11:36:20 +0100

    added particle add-in and changed function to sine function

--- main.py
+++ main.py
@@ -6,35 +6,41 @@ from coord_translation import topol, tocart
 from functions import Mintegral, SMintegral
 from probability import boolout
 
-L = 10
+L = 20
 
-N = 10 * L
+N = 100 * L
 
 def f(x):
-    return 1/(x+1)**2
+    return np.sin(x)**2 /x
 
-mesh = np.linspace(0, L, N)
+mesh = np.linspace(L/N, L, N)
 
 fspace = f(mesh)
+
+plt.plot(mesh, fspace)
+plt.show()
 
 abs = fspace / sum(fspace)
 
 barlist = [0 for i in mesh]
 
-for j in range(2000):
+for j in range(1000):
     for i, e in enumerate(abs):
         test = boolout(e)
         if test == True:
             barlist[i] += 1
+            plt.plot(mesh[i], rd.uniform(0, 1), 'ro')
+
+plt.show()
 
 m = max(barlist)
 
 barlist = np.array(barlist) / m * max(abs)
 
-err = np.sqrt((barlist-abs)**2)
+sqerr = (barlist-abs)**2
 
 plt.plot(mesh, abs)
 plt.plot(mesh, barlist)
 plt.show()
-plt.plot(mesh, err)
+plt.plot(mesh, sqerr)
 plt.show()
