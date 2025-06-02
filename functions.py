ee7a5b55dbc3426ae2258f66f79d3d95e901742a
Sloyfemannen <124448585+Sloyfemannen@users.noreply.github.com>
2025-06-02 14:42:53 +0200

    Moved P, Y, R and L to functions file, solved all of them and normalized them with simpsons

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 
 import numpy as np
+from numpy.polynomial import Polynomial
+from math import comb, factorial, sqrt
 import regex as rg
+import scipy as sp
+from scipy.special import lpmv
+from scipy.integrate import simpson
+import matplotlib.pyplot as plt
 
 def integral(f, x, y, N):
     m = np.linspace(x, y, N)
@@ -21,3 +27,52 @@ def floatint(n):
 
 def randfloat(a, b):
     return None
+
+
+########################
+
+a_0 = 1
+
+def L(a, k, x):
+    return sum([(-1)**i * comb(k+a, k-i) * x**i / factorial(i) for i in range(k+1)])
+
+def P(l, m, x=0):
+    pol = Polynomial([-1, 0, 1])
+    pol = pol ** l
+
+    for i in range(m + l):
+        pol = pol.deriv()
+    
+    return (-1)**m * (1-x**2)**(m/2) * 1/(2**l * factorial(l)) * pol(x)
+
+
+def R(r, n, l):
+    return np.sqrt((2/(n * a_0))**3 * factorial(n - l - 1) / (2 * n * factorial(n + l))) * np.exp(-r / (n * a_0)) * (2 * r / (n * a_0))**l * L(2*l + 1, n-l-1, 2 * r/(n * a_0))
+
+def PY(l, m, theta, phi):
+    norm = sqrt((2 * l + 1) * factorial(l - m) / (4 * np.pi * factorial(l + m)))
+    ex = np.exp(m * phi * 1j)
+    P = lpmv(m, l, np.cos(theta))
+    return norm * P * ex
+
+
+x = np.linspace(0, 100*a_0, 1000)
+
+theta = np.linspace(0, np.pi, 1000)
+phi = np.linspace(0, 2*np.pi, 1000)
+THETA, PHI = np.meshgrid(theta, phi)
+
+for n in range(1, 7):
+    for l in range(0, n):
+        print(f"For n={n}, and l={l}, Simpson's method predicts: ", simpson(R(x, n, l)**2 * x**2, x))
+        print()
+
+for l in range(7):
+    for m in range(-l, l+1):
+        Y = PY(l, m, THETA, PHI)
+        integrand = np.abs(Y)**2 * np.sin(THETA)
+        print("Y: Simpson's method predicts: ", simpson(simpson(integrand, phi, axis=0), theta))
+        print()
+
+def Psi(r, theta, phi, m, l, s):
+    return r**2 * abs(R(r, s, l))**2 * abs(PY(l, m, theta, phi))**2
