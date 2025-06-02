0c3cf0ced4398a0806c2a01567a58515b7682a21
Sloyfemannen <124448585+Sloyfemannen@users.noreply.github.com>
2025-06-02 23:29:50 +0200

    plotting badly

--- main2.py
+++ main2.py
@@ -6,28 +6,58 @@ from math import factorial, comb
 from coord_translation import *
 from functions import *
 from probability import boolout
+import random as rp
+from mpl_toolkits.mplot3d import Axes3D
 
-n = 10
+%matplotlib widget
+
+np.random.seed(19680801)
+
+n = 40
 a_0 = 5.29177210903e-11
 a_0 = 1
 
-X = np.linspace(-10*a_0, 10*a_0, n)
+X = np.linspace(-5*a_0, 5*a_0, n)
 Y = X
 Z = X
 
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-
-
 xs, ys, zs = np.meshgrid(X, Y, Z)
 
-px, py, pz = 
+n = 1
+l = 0
+m = 0
 
-ax.scatter(xs, ys, zs)
+counter = 0
+
+points = []
+for i in range(1):
+    for x in X:
+        for y in Y:
+            for z in Z:
+                if PsiCart(x, y, z, m, l, n) > rp.uniform(0, 1):
+                    points.append(np.array([x, y, z]))
+                if PsiCart(x, y, z, m, l, n) < 1:
+                    counter += 1
+points = np.array(points)
+points = np.transpose(points)
+
+xp = points[0]
+yp = points[1]
+zp = points[2]
+
+Xs, Ys, Zs = np.meshgrid(xp, yp, zp)
+
+fig = plt.figure()
+#ax = fig.add_subplot(projection='3d')
+ax = Axes3D(fig)
+
+
+plot_geeks = ax.scatter(xp, yp, zp)
 
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
 plt.show()
+
+print(counter)
